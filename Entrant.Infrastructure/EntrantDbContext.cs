81b0db50b67aa2daaddc559d624d8f72be3db950
Alex <steelalex.gk@gmail.com>
2022-08-06 16:24:27 +0300

    Added new Infrastructure layer to project  and created DbContext & DbContextSeed

---
+++ Entrant.Infrastructure/EntrantDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Entrant.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Entrant.Infrastructure
+{
+    public sealed class EntrantDbContext : DbContext
+    {
+        public DbSet<EntrantIncedent> Incedents { get; set; }
+        public DbSet<EntrantAccount> Accounts { get; set; }
+        public DbSet<EntrantContact> Contacts { get; set; }
+
+        public EntrantDbContext(DbContextOptions options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(EntrantDbContext).Assembly);
+    }
+
+}
---
+++ Entrant.Infrastructure/EntrantDbContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Entrant.Infrastructure
+{
+    public static class EntrantDbContextSeed
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<EntrantDbContext>());
+            }
+        }
+
+        private static void SeedData(EntrantDbContext entrantDbContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
